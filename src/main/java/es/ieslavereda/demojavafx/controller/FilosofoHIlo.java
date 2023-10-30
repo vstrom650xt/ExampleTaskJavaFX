4d2da893ca6bb764364420b4bcb13dd543097006
vstrom60xt <cerveramonzo@gmail.com>
2023-10-30 20:10:56 +0100

    creo

--- src/main/java/es/ieslavereda/demojavafx/controller/FilosofoHIlo.java
+++ src/main/java/es/ieslavereda/demojavafx/controller/FilosofoHIlo.java
@@ -2,13 +2,13 @@ package es.ieslavereda.demojavafx.controller;
 
 import es.ieslavereda.demojavafx.model.Tenedor;
 
-public class FilosofoHIlo implements  Runnable{
+public class FilosofoHIlo implements  Runnable {
 
     private String name;
     private Tenedor tenedor;
     private Tenedor tenedor2;
 
-    public FilosofoHIlo(String name,Tenedor tenedor, Tenedor tenedor2) {
+    public FilosofoHIlo(String name, Tenedor tenedor, Tenedor tenedor2) {
         this.name = name;
         this.tenedor = tenedor;
         this.tenedor2 = tenedor2;
@@ -16,84 +16,32 @@ public class FilosofoHIlo implements  Runnable{
 
     public void cogerTenedores() {
 
-        if (tenedor.isInUse()){
-            synchronized (tenedor2) {
-                // tenedor esta bloqueado
-                synchronized (tenedor) {
-                    // tenedor 2 esta bloqueado
-                    System.out.println(name + " ha cogido los tenedores y está comiendo.");
-
-                    try {
-                        Thread.sleep(1000);
-                        dejarTenedor();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(name + " ha terminado de comer.");
-                }
-                // tenedor2 deja de estar bloqueado
-            }
-            // tenedor deja de estar bloqueado
-        }else {
+        synchronized (tenedor2) {
+            // tenedor esta bloqueado
             synchronized (tenedor) {
-                // tenedor esta bloqueado
-                synchronized (tenedor2) {
-                    // tenedor 2 esta bloqueado
-                    System.out.println(name + " ha cogido los tenedores y está comiendo.");
-
-                    try {
-                        Thread.sleep(1000);
-                        dejarTenedor();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(name + " ha terminado de comer.");
+                // tenedor 2 esta bloqueado
+                System.out.println(name + " ha cogido los tenedores y está comiendo.");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                // tenedor2 deja de estar bloqueado
+                System.out.println(name + " ha terminado de comer.");
             }
-
+            // tenedor2 deja de estar bloqueado
         }
-
+        // tenedor deja de estar bloqueado
     }
-//        synchronized (tenedor) {
-//            // tenedor esta bloqueado
-//            synchronized (tenedor2) {
-//                // tenedor 2 esta bloqueado
-//                System.out.println(name + " ha cogido los tenedores y está comiendo.");
-//
-//                try {
-//                    Thread.sleep(1000);
-//                    dejarTenedor();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println(name + " ha terminado de comer.");
-//            }
-//            // tenedor2 deja de estar bloqueado
-//        }
-//        // tenedor deja de estar bloqueado
-//    }
 
-//    public void cogerTenedor(){
-//        tenedor.setInUse(true);
-//        tenedor2.setInUse(true);
-//    }
 
-    public void dejarTenedor(){
-        tenedor.setInUse(false);
-        tenedor2.setInUse(false);
-    }
 
 
     public  void siesta(){
-
         try {
-
             Thread.sleep(4000);
         }catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
 
     }
 
@@ -101,7 +49,6 @@ public class FilosofoHIlo implements  Runnable{
     public void run() {
         while (true) {
             cogerTenedores();
-            dejarTenedor();
             siesta();
 
         }
