c2bba4b4ca70c418984e3f1d20f908ff9a738893
cerveramonzo <cerveramonzo@gmail.com>
2023-10-30 14:02:31 +0100

    ññññ

--- src/main/java/es/ieslavereda/demojavafx/controller/FilosofoHIlo.java
+++ src/main/java/es/ieslavereda/demojavafx/controller/FilosofoHIlo.java
@@ -2,71 +2,64 @@ package es.ieslavereda.demojavafx.controller;
 
 import es.ieslavereda.demojavafx.model.Tenedor;
 
-import java.util.concurrent.Semaphore;
+public class FilosofoHIlo implements Runnable {
 
-public class FilosofoHIlo implements  Runnable{
-    private int vecesComidas; // Número de veces que el filósofo ha comido
-    private static Semaphore semaphore = new Semaphore(4); // Un semáforo con 4 permisos
 
     private String name;
     private Tenedor tenedor;
     private Tenedor tenedor2;
 
-    public FilosofoHIlo(String name,Tenedor tenedor, Tenedor tenedor2) {
+     private  int posicion;
+    public FilosofoHIlo(String name, Tenedor tenedor, Tenedor tenedor2) {
         this.name = name;
         this.tenedor = tenedor;
         this.tenedor2 = tenedor2;
-        this.vecesComidas = 5000;
 
     }
 
     public void cogerTenedores() {
-        synchronized (tenedor) {
-            // tenedor esta bloqueado
-            synchronized (tenedor2) {
-                // tenedor2 esta bloqueado
-                System.out.println(name + " ha cogido los tenedores y está comiendo.");
-                try {
-                    tenedor.setInUse(true);
+        if (!tenedor.isInUse()&& !tenedor2.isInUse()){
+            synchronized (tenedor) {
+                tenedor.setInUse(true);
+                // tenedor esta bloqueado
+                synchronized (tenedor2) {
+                    // tenedor2 esta bloqueado
                     tenedor2.setInUse(true);
-                    Thread.sleep(1000);
-                    dejarTenedor();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    System.out.println(name + " ha cogido los tenedores y está comiendo.");
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(name + " ha terminado de comer.");
                 }
-                System.out.println(name + " ha terminado de comer.");
+                // tenedor2 deja de estar bloqueado
+                dejarTenedor(tenedor2);
             }
-            // tenedor2 deja de estar bloqueado
-        }
-        // tenedor deja de estar bloqueado
+            dejarTenedor(tenedor);
+            // tenedor deja de estar bloqueado
+
+        }else{}
+
+
+        siesta();
+
     }
-
-
-    public void dejarTenedor(){
-        tenedor.setInUse(false);
-        tenedor2.setInUse(false);
+    public void dejarTenedor(Tenedor t) {
+        t.setInUse(false);
     }
-
-
-    public  void siesta(){
+    public void siesta() {
         try {
-            Thread.sleep(4000);
-        }catch (InterruptedException e) {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
     @Override
     public void run() {
-
         while (true) {
-
-                cogerTenedores();
-                dejarTenedor();
-                siesta();
-
-
+            cogerTenedores();
+        }
     }
-}}
+}
